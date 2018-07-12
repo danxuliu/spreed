db45e5897ba3857b510166be4aa865ffb3c9036d
Daniel Calviño Sánchez <danxuliu@gmail.com>
2018-07-12 05:42:50 +0200

    Destroy "share:password" rooms as soon as one of the participants leaves
    Due to their nature there is no point in keeping a room to request the
    password of a share once any of the participants has left, so they are
    automatically destroyed in that case.
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -66,6 +66,7 @@ class Application extends App {
 		$this->registerRoomInvitationHook($dispatcher);
 		$this->registerCallNotificationHook($dispatcher);
 		$this->registerChatHooks($dispatcher);
+		$this->registerPublicShareAuthHooks($dispatcher);
 	}
 
 	protected function registerNotifier(IServerContainer $server) {
@@ -272,5 +273,20 @@ class Application extends App {
 			$chatManager->deleteMessages((string) $room->getId());
 		};
 		$dispatcher->addListener(Room::class . '::postDeleteRoom', $listener);
+	}
+
+	protected function registerPublicShareAuthHooks(EventDispatcherInterface $dispatcher) {
+		$listener = function(GenericEvent $event) {
+			/** @var Room $room */
+			$room = $event->getSubject();
+
+			/** @var \OCA\Spreed\PublicShareAuth\Room $publicShareAuthRoom */
+			$publicShareAuthRoom = $this->getContainer()->query(\OCA\Spreed\PublicShareAuth\Room::class);
+			$publicShareAuthRoom->destroyRoomOnParticipantLeave($room);
+		};
+		$dispatcher->addListener(Room::class . '::postRemoveUser', $listener);
+		$dispatcher->addListener(Room::class . '::postRemoveBySession', $listener);
+		$dispatcher->addListener(Room::class . '::postUserDisconnectRoom', $listener);
+		$dispatcher->addListener(Room::class . '::postCleanGuests', $listener);
 	}
 }
